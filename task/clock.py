a78b361281bbcccbb8a23431fcc3c4a770366e0d
najmus140 <najmus@rimiktech.com>
2022-07-23 11:17:09 +0530

    digital clock

---
+++ task/clock.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter.ttk import *
+
+
+from time import strftime
+
+
+root = Tk()
+root.title('DIGITAL CLOCK')
+
+
+def time():
+	string = strftime('%H:%M:%S %p')
+	lbl.config(text = string)
+	lbl.after(300, time)
+
+
+lbl = Label(root, font = ('ds-digital', 25, 'bold'),
+			background = 'green',
+			foreground = 'black')
+
+
+lbl.pack(anchor = 'center')
+time()
+
+mainloop()
